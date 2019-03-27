e952ac1eb9fcbc7fc0d535a4dea3f970ebb0f49a
n0vad3v <n0vad3v@riseup.net>
2019-03-27 23:35:07 +0800

    Yeah, it's working on PyPI

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sfz-novakwok",
+    name="sfz",
     version="0.0.2",
     author="Nova Kwok",
     author_email="n0vad3v@riseup.net",
-    description="A simple Package to deal with Mainland ID Card Number.（一個用來處理大陸身份證號碼的小包）。",
+    description="A simple Package to deal with Mainland ID Card Number.（一个用来处理大陆身份证号码的小包）。",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/n0vad3v/sfz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
